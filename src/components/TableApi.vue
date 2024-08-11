800bdeef77914ec1d7a9805fa8be2c24b7370e96
Victoria <victoria.yamashita@estudante.ufjf.br>
2024-08-11 20:36:36 -0300

    alteracao nos dados da integracao com API

--- src/components/TableApi.vue
+++ src/components/TableApi.vue
@@ -1,37 +1,45 @@
 <template>
-    <v-container>
-      <v-data-table
-        :headers="headers"
-        :items="items"
-      ></v-data-table>
-    </v-container>
+  <v-container>
+    <v-data-table :headers="headers" :items="items"></v-data-table>
+  </v-container>
 </template>
 
 <script setup>
-  import axios from 'axios';
-  import { ref } from 'vue';
+import axios from 'axios';
+import { ref } from 'vue';
 
-  const APIURL = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/MG/municipios';
+const APIURL = 'https://servicodados.ibge.gov.br/api/v3/agregados/4408/periodos/2000%7C2001%7C2002%7C2003%7C2004%7C2005%7C2006%7C2007%7C2008%7C2009%7C2010%7C2011%7C2012%7C2013%7C2014%7C2015%7C2016%7C2017%7C2018%7C2019%7C2020%7C2021%7C2022/variaveis/9734?localidades=N3[31]&classificacao=2[6794]|58[95253]';
 
-  var headers = ref();
-  var items = ref();
+var headers = ref();
+var items = ref();
 
-  axios.get(APIURL)
-    .then(function (response) {
-      headers.value = [
-        { title: 'Id', key: 'id' },
-        { title: 'Município', key: 'name' },
-        { title: 'Constructed', key: 'constructed' },
-        { title: 'Description', key: 'description' },
-      ];
-      items.value = response.data.map(e => { return {
-        id: e.id,
-        name: e.nome,
-        constructed: e.nome,
-        description: e.nome,
-      }})
+axios.get(APIURL)
+  .then(function (response) {
+    console.log("1" + response);
+    headers.value = [
+      { title: 'Id', key: 'id' },
+      { title: 'Ano', key: 'ano' },
+      { title: 'Porcentagem', key: 'porcentagem' },
+    ];
+
+    var qualquer = response.data.map(e => {
+      var resultados = e.resultados[0].series[0].serie;
+
+      const dataArray = Object.entries(resultados).map(([ano, value]) => ({
+        ano: Number(ano),
+        value: parseFloat(value)
+      }));
+
+      items.value = dataArray.map((x, index) => {
+        return {
+          id: index + 1,
+          ano: x.ano,
+          porcentagem: x.value,
+        }
+      })
     })
-    .catch(function (error) {
-      console.error(error);
-    });
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
 </script>
